90b00808e24af2520e3222410cb95e2e805e9543
Linus <linus.osthof@gmx.de>
2020-03-07 20:26:47 +0100

    Commiting working tree to make sure nothing gets lost.

--- recurrent.py
+++ recurrent.py
@@ -19,7 +19,7 @@ song5 = mh.extract_bars('./inputs/inp_5_cMaj_4-4th_temp_1.mid')
 
 songs = np.array(song1 + song2 + song3 + song4 + song5).flatten()
 
-print("songs", songs)
+#print("songs", songs)
 
 def get_batch(song_list, seq_length, batch_size):
     # get length of inputs and randomly take subset
@@ -31,8 +31,8 @@ def get_batch(song_list, seq_length, batch_size):
     #create batches in proper size to feed to rnn
     x_batch = np.reshape(input_batch, [batch_size, seq_length])
     y_batch = np.reshape(output_batch, [batch_size, seq_length])
-    print("seq_len", seq_length, "batch_size", batch_size)
-    print("x_batch", x_batch, "y_batch", y_batch)
+    #print("seq_len", seq_length, "batch_size", batch_size)
+    #print("x_batch", x_batch, "y_batch", y_batch)
     return x_batch, y_batch
 
 x_batch, y_batch = get_batch(songs, 5, 1)
@@ -95,40 +95,43 @@ def train_step(x, y):
 history = []
 
 # run the network for a while
-"""vfor iter in tqdm(range(num_training_iterations)):
+for iter in tqdm(range(num_training_iterations)):
     x_batch, y_batch = get_batch(songs, seq_length, batch_size)
-    print(x_batch.shape, y_batch.shape)
+    #print(x_batch.shape, y_batch.shape)
     loss = train_step(x_batch, y_batch)
 
     history.append(loss.numpy().mean())
 
     if iter % 100 == 0:     
       model.save_weights(checkpoint_prefix)
-"""
-def generate_song(model, start_string, generation_length=1000):
+
+def generate_song(model, start_string, generation_length=100):
+    current_string = tf.expand_dims(start_string, 0)
+    #print("111111111", current_string)
     song_generated = []
-    current_string = start_string
-    input_eval = tf.expand_dims(current_string, 0)
     # Here batch size == 1
     model.reset_states()
     #tqdm._instances.clear()
     for i in range(generation_length):
-      predictions = model(current_string)
-      predictions = tf.squeeze(predictions, 0)
-      #dont think I need this
-      #predicted_id = tf.random.categorical(predictions, num_samples=1)[-1,0].numpy()
-      #song_generated = tf.expand_dims([predicted_id], 0)
-      current_string = predictions
-      song_generated.append(predictions)
+        predictions = model(current_string)
+        #print("p1", predictions, predictions.shape)
+        predictions = tf.squeeze(predictions, 0)
+        #print("p2", predictions, predictions.shape)
+        predicted_id = tf.random.categorical(predictions, num_samples=1)[-1,0].numpy()
+        #print("p3", predicted_id)
+        current_string = tf.expand_dims([predicted_id],0)
+        song_generated.append(predicted_id)
 
-    return (start_string + ''.join(song_generated))
+    return (np.array(start_string + song_generated).flatten())
 
 model = build_model(vocab_size, embedding_dim, rnn_units, batch_size=1)
-
 # Restore the model weights for the last checkpoint after training
 model.load_weights(tf.train.latest_checkpoint(checkpoint_dir))
 model.build(tf.TensorShape([1, None]))
 
-generated_song = generate_song(model, start_string=np.array([64]), generation_length=100)
+model.summary()
 
-mh.write_to_file(generate_song, mid.ticks_per_beat, filepath="./", filename="rnn_recon.mid")
+generated_song = generate_song(model, start_string=[64], generation_length=159)
+#print("gen_sng", generated_song, generated_song.shape)
+generated_song = generated_song.reshape((16, -1))
+mh.write_to_file(generated_song.tolist(), mid.ticks_per_beat, filepath="./", filename="rnn_recon", stretch=False)
